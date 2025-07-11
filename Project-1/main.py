2a2945777334fe2e630ec68e04f7ac68eee892f7
nnaman123 <sweta.singh21@gmail.com>
2025-07-11 20:36:27 +0530

    first commit

---
+++ Project-1/main.py
@@ -0,0 +1,15 @@
+#storing values
+tree1 = 254
+tree2 = 100
+tree3 = 300
+tree4 = 150
+tree5 = 200
+tree6 = 350
+#calculating the average
+average = (tree1 + tree2 + tree3 + tree4 + tree5 + tree6) / 6
+#displaying the average
+print("The average height of the trees is:", average)
+#finding the total of the trees
+total = tree1 + tree2 + tree3 + tree4 + tree5 + tree6
+#displaying the total
+print("The total height of the trees is:", total)
